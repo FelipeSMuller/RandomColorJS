5339ad306d45e7a418397245d582bf3ff2b0447b
Felipe Müller <felipesilvamuller@Hotmail.com>
2022-11-21 16:52:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//Cores armazenadas
+const cores = [
+    "Blue",
+    "Red", 
+    "Green" ,
+    "Gray",
+    "Purple",
+    "Orange",
+    "Black",
+    ];
+//Event Listener
+const botao = document.querySelector('.btn');
+botao.addEventListener('click',function(){
+    var randomColor = cores[Math.floor(Math.random()*cores.length)];
+    let header = document.getElementById('mudaCor');
+    header.style.backgroundColor = randomColor;
+});
+
